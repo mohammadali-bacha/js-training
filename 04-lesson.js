8712d63d13e62926196f5023d1848e1e4ea3a65f
Karim Aouadi <kdolphinus@gmail.com>
2018-12-19 02:47:35 +0100

    Updated lesson 4

--- 04-lesson.js
+++ 04-lesson.js
@@ -1,9 +1,9 @@
 
 /*
 
-L'exercice consiste a parcourir le tableau 
-et rechercher les valeurs divisibles par 2 
-puis les remplacer par un multiple de 3
+L'exercice consiste a parcourir le tableau et 
+rechercher les valeurs divisibles 
+par 2 puis les remplacer par un multiple de 3
 
 */
 
@@ -23,10 +23,10 @@ Utilisation d'une boucle for pour parcourir le tableau de valeur
 
 for (var i = 0; i < lg;) {
     /*
-      La condition verifie si la valeur en ieme position tab[i] 
-      dans le tableau est divisible par 2.
-      Si la condition est verifié la valeur en ieme position tab[i]
-      est mis à jour avec la valeur en ieme position multiplié par 3
+    La condition verifie si la valeur en ieme position tab[i] 
+    dans le tableau est divisible par 2.
+    Si la condition est verifié la valeur en ieme position tab[i]
+    est mis à jour avec la valeur en ieme position multiplié par 3
     */
     if (tab[i] % 2 == 0 ){
         tab[i]= tab[i] * 3;
--- complements.js
+++ complements.js
@@ -1,7 +1,8 @@
 /* TODO :
 definir : 
 
--integer : Vous pouvez stocker des nombres dans des variables, soit des nombres entiers comme 30 ou des nombres décimaux comme 2.456 (appelés aussi nombres à virgule flottante). 
+-integer : Vous pouvez stocker des nombres dans des variables, 
+ soit des nombres entiers comme 30 ou des nombres décimaux comme 2.456 . 
 
 - decimal :
 
