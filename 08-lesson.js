37a78331bd0db2668baa9acd8ce9807382c632da
m-ali <bacha@tuta.io>
2018-12-27 18:39:29 +0100

    changes

--- 08-lesson.js
+++ 08-lesson.js
@@ -4,10 +4,9 @@ L'exercice consiste a rechercher la plus petite valeur dans un tableau
 /* 
 Initialisation variable d'un tableau
 */
-var tableau = [3, 4, 6, 2, 1, 7, 5, 8];
 
 // dé-commenter pour tester l'algorithme avec d'autres valeurs :
-// var tableau = [25,47,32,18,56]; 
+var tableau = [25,47,32,18,56]; 
 var miseSurEtagere = tableau[0];
 /* 
 Recuperation de la longeur du tableau
@@ -23,12 +22,12 @@ for (var index = 0; index <= longueur; index++) {
     }
     /*
     La condition verifie si la valeur en ieme position tableau[index] 
-    dans le tableau est plus petite que la positon index+1 qui est l
+    dans le tableau est plus petite que la positon index+1 qui est longueur
     Si la condition est verifiée la valeur en ieme position tableau[index]
-    est enregistree dans la variable k et la variable l est incrementee.
+    est enregistree dans la variable miseSurEtagere et la variable longueur est incrementee.
     */
     /*
-    Sinon l'ieme position tableau[l] est enregistree dans la variable k.
+    Sinon l'ieme position tableau[index] est enregistree dans la variable miseSurEtagere.
         */
     // }
 }
