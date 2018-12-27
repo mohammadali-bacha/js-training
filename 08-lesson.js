dafac62ebaf387721f530cb7bdc0346edc4cf0d3
m-ali <bacha@tuta.io>
2018-12-27 18:36:54 +0100

    changes

--- 08-lesson.js
+++ 08-lesson.js
@@ -8,30 +8,27 @@ var tableau = [3, 4, 6, 2, 1, 7, 5, 8];
 
 // dé-commenter pour tester l'algorithme avec d'autres valeurs :
 // var tableau = [25,47,32,18,56]; 
-var mise_sur_etagere = tableau[0];
-var compteur = 1;
+var miseSurEtagere = tableau[0];
 /* 
 Recuperation de la longeur du tableau
 */
 var longueur = tableau.length;
-tableau.length = longueur;
 
 /*
 Utilisation d'une boucle for pour parcourir le tableau de valeur  
 */
-for (var index = 0; index <= longueur; index++){
-    if (mise_sur_etagere > tableau[index]){
-        mise_sur_etagere = tableau[index]
+for (var index = 0; index <= longueur; index++) {
+    if (miseSurEtagere > tableau[index]) {
+        miseSurEtagere = tableau[index]
     }
-    // if (tableau[index] < tableau[compteur]) {
-            /*
-    La condition verifie si la valeur en ieme position arr[index] 
+    /*
+    La condition verifie si la valeur en ieme position tableau[index] 
     dans le tableau est plus petite que la positon index+1 qui est l
-    Si la condition est verifiée la valeur en ieme position arr[index]
+    Si la condition est verifiée la valeur en ieme position tableau[index]
     est enregistree dans la variable k et la variable l est incrementee.
     */
-        /*
-    Sinon l'ieme position arr[l] est enregistree dans la variable k.
+    /*
+    Sinon l'ieme position tableau[l] est enregistree dans la variable k.
         */
     // }
 }
@@ -40,6 +37,4 @@ for (var index = 0; index <= longueur; index++){
 console.log(tableau);
 // Affichage de la valeur la plus petite du tableau
 
-console.log("La valeur la plus petite est " + mise_sur_etagere + ".");
-
-
+console.log("La valeur la plus petite est " + miseSurEtagere + ".");
