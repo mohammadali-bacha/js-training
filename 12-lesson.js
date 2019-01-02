0ce0c176b7daebafbe80474d5bc614d3c4483eab
m-ali <bacha@tuta.io>
2019-01-02 15:43:23 +0100

    lesson 12 and 13

--- 12-lesson.js
+++ 12-lesson.js
@@ -5,20 +5,23 @@ Le but de l'exercice est de faire une fonction retournant le resultat de l'addit
 
 var tableau = [12,34,56,78,90];
 // la somme de ce tableau est de 270.
-var miseSurEtagere = null;
+var miseSurEtagere = tableau[0];
 var somme = null;
 var longueur = tableau.length;
 
 function addition(tableau) {
-   for (var index = 0 ; index < longueur; index++) {
+   for (var index = 1 ; index < longueur; index++) {
+       console.log("index : " + index);    
         miseSurEtagere = tableau[index];
+        console.log("miseSurEtagere : " + miseSurEtagere);  
+        console.log("valeur du tableau"+tableau[index]);           
         somme = tableau[index] + miseSurEtagere;
-        return console.log(somme);; 
+        return console.log("La somme est de " + somme);; 
     }
 }
 
-var tableau1 = [23,46,12,34];
+var tableau1 = [23,46,12,34];          
 // la somme de ce tableau est de 115.
-sum = addition(tableau1);
+var sum = addition(tableau1);
 
 console.log("La somme du tableau 1 est de : " + sum);
--- 13-lesson.js
+++ 13-lesson.js
@@ -2,3 +2,15 @@
 Le but de l'exercice est de regrouper les nombres de meme valeurs d'un tableau dans un autre tableau en les additionnant.
 */
 
+var tableauEntree = [100,200,300,100,50,50,100];
+var tableauSortie = [];
+var longueur = tableau.length;
+for (var index = 0; index < tableau; index++){
+    for (){
+
+        return 
+    }
+     
+    return console.log(tableauSortie);
+    
+}
