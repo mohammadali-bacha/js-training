a4111f2886160465f7809e0c73c116c4c879c32e
m-ali <bacha@tuta.io>
2019-01-05 23:45:30 +0100

    lesson18 finish

--- 18-lesson.js
+++ 18-lesson.js
@@ -7,17 +7,17 @@ Remplacer toutes les valeurs divisibles par 2, leur ajouter +1.
 
 function valeursDivisiblePar2(tableau){
     // var miseSurEtagere = tableau[index];
-for (var index = 0; index < tableau.length; index++) {
+for (var index = 0; index < tableau.length; index++) {    
     if (tableau[index] % 2 == 0) {
-        tableau[index] = tableau[index]+1;
-        console.log(tableau[index]);
-        
+        tableau[index] = tableau[index]+1;      
     }
 }
 return tableau;
 }
 
-var tableau = [23,67,43,98,21,13];
+// var tableau = [23,67,43,98,21,13];
+var tableau = [2,4,6,8,10,21];
+console.log(tableau);
 
 var resultat = valeursDivisiblePar2(tableau);
 
