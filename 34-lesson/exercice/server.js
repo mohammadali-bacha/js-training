b8d0f7947167314e79c4a49c880394980d944d2e
m-ali <bacha@tuta.io>
2019-02-01 15:34:27 +0100

    add nodejs

--- 34-lesson/exercice/server.js
+++ 34-lesson/exercice/server.js
@@ -4,11 +4,12 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.listen(1900, function (){
-    console.log("serveur running on port " + this.address().port); 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.get('*', (request, response) => {
+    console.log(request.url);
 });
-
-
-
+app.listen(1900, function () {
+    console.log("serveur running on port " + this.address().port);
+});
